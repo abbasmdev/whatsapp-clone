2abffa30447cc8e622379b7f06ba9feea992a979
Abbas Moharami <abbasmdev@gmail.com>
2021-06-23 07:24:32 +0430

    1. Added firebase hooks package 2. Changed App.js to work better when login state changed.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { firebaseAuth, firebaseDb } from "./firebase/firebase";
 import { Switch, Route, Redirect, useHistory } from "react-router-dom";
 import HomePage from "./pages/Home";
@@ -6,39 +7,42 @@ import AuthPage from "./pages/Auth";
 import styles from "./App.module.css";
 
 function App() {
+  const [user, loading, error] = useAuthState(firebaseAuth);
   const history = useHistory();
   useEffect(() => {
-    firebaseAuth.onAuthStateChanged((user) => {
-      if (!user) {
-        history.replace("/auth");
-      }
-      firebaseDb.collection("users").doc(user.uid).set(
-        {
-          email: user.email,
-          photoURL: user.photoURL,
-        },
-        { merge: true }
-      );
-      history.replace("/");
-    });
-    return () => {
-      firebaseAuth.onAuthStateChanged(null);
-    };
-  }, [history]);
+    if ((!user && !loading) || (!loading && error)) {
+      history.replace("/auth");
+    }
+    if (!user && loading) return;
+
+    console.log(user);
+    firebaseDb.collection("users").doc(user.uid).set(
+      {
+        email: user.email,
+        photoURL: user.photoURL,
+      },
+      { merge: true }
+    );
+    history.replace("/");
+  }, [history, user, loading, error]);
 
   return (
     <div className={styles.app}>
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
-        <Route path="/auth">
-          <AuthPage />
-        </Route>
-        <Route path="*">
-          <Redirect to="/" />
-        </Route>
-      </Switch>
+      {loading ? (
+        <p>Loading</p>
+      ) : (
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      )}
     </div>
   );
 }
