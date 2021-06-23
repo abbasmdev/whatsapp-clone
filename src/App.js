5fbeaa868bbd158266a56cd0c48d9e168e0f18d9
Abbas Moharami <abbasmdev@gmail.com>
2021-06-23 06:44:33 +0430

    1. Complete Login page 2. Add login functionality 3. Change firebase to return google sigin provider. 4. Updated App style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import { firebaseAuth, firebaseDb } from "./firebase/firebase";
+import { Switch, Route, Redirect, useHistory } from "react-router-dom";
 import HomePage from "./pages/Home";
 import AuthPage from "./pages/Auth";
 import styles from "./App.module.css";
 
 function App() {
+  const history = useHistory();
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged((user) => {
+      if (!user) {
+        history.replace("/auth");
+      }
+      firebaseDb.collection("users").doc(user.uid).set(
+        {
+          email: user.email,
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      );
+      history.replace("/");
+    });
+    return () => {
+      firebaseAuth.onAuthStateChanged(null);
+    };
+  }, [history]);
+
   return (
     <div className={styles.app}>
       <Switch>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,5 +5,6 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export const firebaseAuth = firebaseApp.auth();
 export const firebaseDb = firebaseApp.firestore();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export default firebaseApp;
